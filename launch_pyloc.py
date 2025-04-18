17866599d9032b6bc78361f478a27c764a1673fa
Miguel Parra <migparra73@gmail.com>
2025-04-18 11:48:04 -0700

    Fixed linux segfault problem

--- launch_pyloc.py
+++ launch_pyloc.py
@@ -3,8 +3,14 @@
 __author__ = 'iped'
 import os
 
+
 from traits.etsconfig.api import ETSConfig
 ETSConfig.toolkit = 'qt'
+
+# If linux, set the QT_QPA_PLATFORM environment variable to xcb
+if os.name == 'posix':
+    if 'QT_QPA_PLATFORM' not in os.environ:
+        os.environ['QT_QPA_PLATFORM'] = 'xcb'
 
 from view.pyloc import PylocControl
 import yaml
